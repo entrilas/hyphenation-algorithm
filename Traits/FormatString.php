ac1657b84b9590f9226d449885c7a37c8efcc84a
entrilas <53130677+entrilas@users.noreply.github.com>
2022-02-22 15:26:50 +0200

    changes

---
+++ Traits/FormatString.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Traits;
+
+trait FormatString
+{
+    public function removeNumbers(string $word): string
+    {
+        return preg_replace('/\d/', '', $word);
+    }
+
+    public function clearString(string $word): string
+    {
+        $word = $this->removeSymbols($word);
+        $word = $this->removeNumbers($word);
+        return $this->removeSpaces($word);
+    }
+
+    public function removeSpaces(string $word): string
+    {
+        return preg_replace('/\s+/', '', $word);
+    }
+
+    public function splitSentenceIntoWords(string $sentence): array
+    {
+        return preg_split('/\s+/', $sentence, -1, PREG_SPLIT_NO_EMPTY);
+    }
+
+    public function removeSymbols(string $word): string
+    {
+        return trim($word);
+    }
+}
